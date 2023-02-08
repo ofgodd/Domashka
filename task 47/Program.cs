ef4678ee82cc8aea6ca9ae384105e17e2ff8eedc
Arkady <djony23rus@mail.ru>
2023-02-08 22:33:49 +0200

    Сделал 8.02

---
+++ task 47/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+void FillArray(double [,]array, int String, int column)
+{
+    for (int i = 0; i < String; i++)
+        {
+            for (int j = 0; j < column; j++)
+                 {
+                    array[i,j] = Math.Round (new Random().Next(-10,10) + new Random().NextDouble(), 2 ); 
+                    Console.Write(array[i,j] + " ");
+                 }
+                 Console.WriteLine();
+                    
+                    
+        }
+}   Console.Write($"Ваш массив " );
+Console.Write("Введите число строк массива ");
+int String = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов массива ");
+int column = Convert.ToInt32(Console.ReadLine());
+double [,] array = new double [String , column];
+FillArray(array, String, column);
