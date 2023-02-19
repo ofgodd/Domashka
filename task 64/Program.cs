476ae618eeeb25bdcd56aa69386f90f277eef14d
Arkady <djony23rus@mail.ru>
2023-02-19 21:56:14 +0200

    Сделал 64

---
+++ task 64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void PrintNumbers(int number) 
+{   
+    if (number == 0)
+        {
+            return;
+        }
+    else 
+        {
+            Console.Write(number + " ");
+        }
+PrintNumbers(number - 1);
+}
+Console.Write("Введите число последовательности убывания к 1 : вводимое число  ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 0)
+{
+    Console.Write("Вы ввели отрицительно число!!! Пожалуйста перезапустите программу и введите число больше 0 ");
+}
+else
+{
+    PrintNumbers(N);
+}
